b5c4ff489ce79ae1b8937eddd6d812e4339acf94
MOHAMMAD HAJJAR <98041736+mohhaj-77@users.noreply.github.com>
2022-10-04 21:41:24 +0300

    doktor

---
+++ doktor website/doktor/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbars from './component/navbar.JS';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+  return (
+    <div>
+      
+    <Navbars />
+
+    </div>
+
+  );
+}
+
+export default App;
